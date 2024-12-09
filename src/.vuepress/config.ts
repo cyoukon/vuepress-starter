8c9e37eda788c5b54925f08865edf8d1102099c2
cyoukon <z1162289133@gmail.com>
2024-12-09 15:08:11 +0800

    更换框架 npm init vuepress-theme-hope@latest my-docs

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,27 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
+
+  theme,
+
+  host: '127.0.0.1',
+  port: 8080
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
