09e5da221a47ff6ee6963d88f874e925261057fc
cyoukon <z1162289133@gmail.com>
2024-12-09 22:47:41 +0800

    修改base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vuepress-starter/",
   
   host: '127.0.0.1',
   port: 8080,
